c1762c7e917b57c1bdaaeab06450282aa381f4c6
Coder200212 <coder@glynac.ai>
2025-05-21 14:47:51 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseStaticFiles();
-app.MapGet("/", () => "Hello from ASP.NET Core!");
+app.MapGet("/", () => "Hello from Jonz GT!");
 
 app.Run();
