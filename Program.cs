7b2c344b22192d0df581ec51663653fed2bdbed7
Coder200212 <coder@glynac.ai>
2025-05-21 15:02:36 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.UseStaticFiles();
-app.MapGet("/", () => "Hello from ASP.NET Core!");
-
-app.Run();
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Enable serving static files from wwwroot
 app.UseStaticFiles();
-
-// Route root ("/") to index.html
 app.MapFallbackToFile("index.html");
 
 app.Run();
