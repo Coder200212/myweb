2c81d660441981b074e7b04fb3d07ab6fc0f778d
Coder200212 <coder@glynac.ai>
2025-05-21 14:54:22 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,3 +6,13 @@ app.MapGet("/", () => "Hello from ASP.NET Core!");
 
 app.Run();
 
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Enable serving static files from wwwroot
+app.UseStaticFiles();
+
+// Route root ("/") to index.html
+app.MapFallbackToFile("index.html");
+
+app.Run();
